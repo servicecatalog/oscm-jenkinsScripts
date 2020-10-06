4b7537f72bb3fdaf6e82fbbc810a5b67c4a1a200
kabaciski <piotr.kabacinski@ts.fujitsu.com>
2020-10-06 18:03:42 +0200

    fixup! Fix sass dwonload

--- updates/oscm-core.groovy
+++ updates/oscm-core.groovy
@@ -78,7 +78,7 @@ def execute() {
             sh "mkdir ${WORKSPACE}/apache-ant && tar -xf ${WORKSPACE}/apache-ant.tar.gz -C ${WORKSPACE}/apache-ant --strip-components 1"
             sh "wget https://repo1.maven.org/maven2/org/apache/ivy/ivy/2.4.0/ivy-2.4.0.jar -O ${WORKSPACE}/apache-ant/lib/ivy.jar"
             sh "wget https://github.com/sass/dart-sass/releases/download/1.26.11/dart-sass-1.26.11-linux-x64.tar.gz -O ${WORKSPACE}/dart-sass-1.26.11-linux-x64.tar.gz"
-            sh "mkdir ${WORKSPACE}/sass && tar -xf ${WORKSPACE}/tmp/dart-sass-1.26.11-linux-x64.tar.gz -C ${WORKSPACE}/sass --strip-components 1"
+            sh "mkdir ${WORKSPACE}/sass && tar -xf ${WORKSPACE}/dart-sass-1.26.11-linux-x64.tar.gz -C ${WORKSPACE}/sass --strip-components 1"
 
             ANT_BIN = sh(
                     script: "echo ${WORKSPACE}/apache-ant/bin/ant",
