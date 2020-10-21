c77a5ed7e9af45df650115dd7e865e2115ca0ce2
cworf91 <christian.worf@est.fujitsu.com>
2020-10-21 16:27:09 +0200

    quickfix

--- shared/cleanup.groovy
+++ shared/cleanup.groovy
@@ -75,7 +75,7 @@ void execute() {
             mkdir -p ${WORKSPACE}
             if [ ${COMPLETE_CLEANUP} == "true" ]; then
                 docker run --rm -v ${WORKSPACE}/docker/data/oscm-db:/db busybox rm -rf /db/data || true;
-                docker run --rm -v ${WORKSPACE}:/workspace centos:7 find /workspace -uid 0 -delete
+                docker run --rm -v ${WORKSPACE}:/workspace estartifactory.intern.est.fujitsu.com:445/oscm-registry/oscm-centos:7 find /workspace -uid 0 -delete
                 rm -rf ${WORKSPACE}/*
                 rm -rf ${WORKSPACE}/{,.[!.],..?}*
                 mkdir ${WORKSPACE}/docker
