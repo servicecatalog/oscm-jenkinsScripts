7ce629fe688670d21dd0b7114103bda11df22ff4
cworf91 <christian.worf@est.fujitsu.com>
2020-11-05 14:17:46 +0100

    tag images

--- shared/build-core.groovy
+++ shared/build-core.groovy
@@ -97,6 +97,11 @@ def execute() {
              docker.image("${DOCKER_REGISTRY}/${DOCKER_ORGANIZATION}/oscm-gc-ant:${DOCKER_TAG}").pull()
              docker.image("${DOCKER_REGISTRY}/${DOCKER_ORGANIZATION}/oscm-centos-based:${DOCKER_TAG}").pull()
              docker.image("${DOCKER_REGISTRY}/${DOCKER_ORGANIZATION}/oscm-maven:${DOCKER_TAG}").pull()
+             sh(
+                'docker tag ${DOCKER_REGISTRY}/${DOCKER_ORGANIZATION}/oscm-gc-ant:${DOCKER_TAG} gc-ant; ' 
+                docker tag ${DOCKER_REGISTRY}/${DOCKER_ORGANIZATION}/oscm-centos-based:${DOCKER_TAG} oscm-centos-based; ' 
+                docker tag ${DOCKER_REGISTRY}/${DOCKER_ORGANIZATION}oscm-maven:${DOCKER_TAG} oscm-maven; ' 
+            )
         }
     }
 
