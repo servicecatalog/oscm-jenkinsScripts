19c22b37e3017e4c86ded2a74659835bc39278f5
cworf91 <christian.worf@est.fujitsu.com>
2020-11-20 08:48:34 +0100

    refactor

--- test-webservices.groovy
+++ test-webservices.groovy
@@ -82,69 +82,19 @@
 // Description: Load sample data?
 
 node("${NODE_NAME}") {
-
-    def _prepareBuildTools = {
-        stage('Build - pull build tools') {
-             docker.image("${DOCKER_REGISTRY}/${DOCKER_ORGANIZATION}/oscm-gc-ant:${DOCKER_TAG}").pull()
-             docker.image("${DOCKER_REGISTRY}/${DOCKER_ORGANIZATION}/oscm-centos-based:${DOCKER_TAG}").pull()
-             docker.image("${DOCKER_REGISTRY}/${DOCKER_ORGANIZATION}/oscm-maven:${DOCKER_TAG}").pull()
-             docker.image("${DOCKER_REGISTRY}/${DOCKER_ORGANIZATION}/oscm-gf:${DOCKER_TAG}").pull()
-             sh(
-                'docker tag ${DOCKER_REGISTRY}/${DOCKER_ORGANIZATION}/oscm-gc-ant:${DOCKER_TAG} gc-ant; ' +
-                'docker tag ${DOCKER_REGISTRY}/${DOCKER_ORGANIZATION}/oscm-centos-based:${DOCKER_TAG} oscm-centos-based; ' +
-                'docker tag ${DOCKER_REGISTRY}/${DOCKER_ORGANIZATION}/oscm-gf:${DOCKER_TAG} oscm-gf; ' +
-                'docker tag ${DOCKER_REGISTRY}/${DOCKER_ORGANIZATION}/oscm-maven:${DOCKER_TAG} oscm-maven; ' 
-            )
-        }
-    }
-    
-        def _downloadLibraries = {
-        stage('Build - download external libraries') {
-            sh "docker run " +
-                    "--name gc-ant-ivy-${BUILD_ID} " +
-                    "--rm " +
-                    "-v ${WORKSPACE}:/build " +
-                    "-e http_proxy=\"${http_proxy}\" " +
-                    "-e https_proxy=\"${http_proxy}\" " +
-                    "-e HTTP_PROXY=\"${http_proxy}\" " +
-                    "-e HTTPS_PROXY=\"${http_proxy}\" " +
-                    "-e ANT_OPTS=\"-Dhttp.proxyHost=proxy.intern.est.fujitsu.com -Dhttp.proxyPort=8080 -Dhttps.proxyHost=proxy.intern.est.fujitsu.com -Dhttps.proxyPort=8080\" " +
-                    "gc-ant -f /build/oscm-devruntime/javares/build-oscmaas.xml BUILD.LIB"
-        }
-    }
-    
-        def _compileCore = {
-        stage('Build - compile oscm-core') {
-            user = sh(returnStdout: true, script: 'id -u').trim()
-            group = sh(returnStdout: true, script: 'id -g').trim()
-            sh "docker run " +
-                    "--name gc-ant-core-${BUILD_ID} " +
-                    "--user $user:$group " +
-                    "--rm " +
-                    "-v ${WORKSPACE}:/build " +
-                    "-e http_proxy=\"${http_proxy}\" " +
-                    "-e https_proxy=\"${https_proxy}\" " +
-                    "-e HTTP_PROXY=\"${http_proxy}\" " +
-                    "-e HTTPS_PROXY=\"${https_proxy}\" " +
-                    "-e ANT_OPTS=\"-Dhttp.proxyHost=proxy.intern.est.fujitsu.com -Dhttp.proxyPort=8080 -Dhttps.proxyHost=proxy.intern.est.fujitsu.com -Dhttps.proxyPort=8080\" " +
-                    "-e PATH=/usr/local/dart-sass:${env.PATH} " +
-                    "gc-ant -f /build/oscm-devruntime/javares/build-oscmaas.xml BUILD.BES"
-        }
-    }
     
     
     def clean = evaluate readTrusted('shared/cleanup.groovy')
     def pull = evaluate readTrusted('shared/pull.groovy')
     def checkoutTests = evaluate readTrusted('shared/checkout-tests.groovy')
+    def prepareWS = evaluate readTrusted('shared/prepare-ws.groovy')
     def start = evaluate readTrusted('shared/start.groovy')
     def test = evaluate readTrusted('shared/test-webservices.groovy')
 
     clean.execute()
     pull.execute()
-    _prepareBuildTools()
     checkoutTests.execute()
-    _downloadLibraries()
-    _compileCore()
+    prepareWS.execute()
     start.execute()
     test.execute()
     clean.execute()
