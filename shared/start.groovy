66d499458387f7f2f67d2c22d3b6d771b6eebf1e
cworf91 <christian.worf@est.fujitsu.com>
2020-12-09 09:59:18 +0100

    add script

--- shared/start.groovy
+++ shared/start.groovy
@@ -50,6 +50,8 @@ void execute(String FQDN = env.NODE_NAME + '.intern.est.fujitsu.com') {
             // If the docker dir doesn't exists at this point, it is created with root owner by volume mapping with the docker run command
             // But we need to change it's owner to jenkins here, in order that env files can be replaced with sed later on
             // (this is because sed needs permission to create a temp file in this directory!)
+            script {
+            env.FQDN_NODE = FQDN
             sh '''
             docker run --rm -v ${WORKSPACE}/docker:/docker busybox chown $(id -u jenkins):$(id -g jenkins) /docker
             mkdir -p ${WORKSPACE}/docker/config/oscm-identity/tenants/
@@ -61,7 +63,7 @@ void execute(String FQDN = env.NODE_NAME + '.intern.est.fujitsu.com') {
                 -e HOST_FQDN=${FQDN} \
                 ${DOCKER_REGISTRY}/${DOCKER_ORGANIZATION}/oscm-deployer:${DOCKER_TAG}
             '''
-         
+         }
         }
     }
 
