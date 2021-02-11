09fddd864cada149b3eac151400322f9dd55b708
cworf91 <christian.worf@est.fujitsu.com>
2021-02-11 17:56:53 +0100

    update secret

--- tests/portal-integration-tests.groovy
+++ tests/portal-integration-tests.groovy
@@ -52,7 +52,7 @@ def execute() {
             sed -i \
                 -e "s|^\\(oidc.provider\\+=\\).*|\\1default|g" \
                 -e "s|^\\(oidc.clientId\\+=\\).*|\\1fd8194f8-6f45-4210-bfcb-647a0e438918|g" \
-                -e "s|^\\(oidc.clientSecret\\+=\\).*|\\1788_tkQ1-LKSio2U3lm4Q~4Xc3K4eZ~6_Y|g" \
+                -e "s|^\\(oidc.clientSecret\\+=\\).*|\\1buA_sBZmbKVUSa~8n57koKOM-_i0UUII8_|g" \
                 -e "s|^\\(oidc.authUrl\\+=\\).*|\\1https://login.microsoftonline.com/ctmgsso.onmicrosoft.com/oauth2/v2.0/authorize|g" \
                 -e "s|^\\(oidc.logoutUrl\\+=\\).*|\\1https://login.microsoftonline.com/ctmgsso.onmicrosoft.com/oauth2/v2.0/logout|g" \
                 -e "s|^\\(oidc.tokenUrl\\+=\\).*|\\1https://login.microsoftonline.com/ctmgsso.onmicrosoft.com/oauth2/v2.0/token|g" \
