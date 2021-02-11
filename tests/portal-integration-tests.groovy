523c8b7209dbf67cadacf187c07087ece0aba62f
cworf91 <christian.worf@est.fujitsu.com>
2021-02-11 17:31:46 +0100

    Use OSCM11 application

--- tests/portal-integration-tests.groovy
+++ tests/portal-integration-tests.groovy
@@ -51,8 +51,8 @@ def execute() {
                 sh '''
             sed -i \
                 -e "s|^\\(oidc.provider\\+=\\).*|\\1default|g" \
-                -e "s|^\\(oidc.clientId\\+=\\).*|\\1ef29aa22-369b-424f-9c72-6800fb24239d|g" \
-                -e "s|^\\(oidc.clientSecret\\+=\\).*|\\1ARNRWfZsWiA6A]=tmA-GyRPlhX9=9VQ7|g" \
+                -e "s|^\\(oidc.clientId\\+=\\).*|\\1fd8194f8-6f45-4210-bfcb-647a0e438918|g" \
+                -e "s|^\\(oidc.clientSecret\\+=\\).*|\\1788_tkQ1-LKSio2U3lm4Q~4Xc3K4eZ~6_Y|g" \
                 -e "s|^\\(oidc.authUrl\\+=\\).*|\\1https://login.microsoftonline.com/ctmgsso.onmicrosoft.com/oauth2/v2.0/authorize|g" \
                 -e "s|^\\(oidc.logoutUrl\\+=\\).*|\\1https://login.microsoftonline.com/ctmgsso.onmicrosoft.com/oauth2/v2.0/logout|g" \
                 -e "s|^\\(oidc.tokenUrl\\+=\\).*|\\1https://login.microsoftonline.com/ctmgsso.onmicrosoft.com/oauth2/v2.0/token|g" \
@@ -66,7 +66,6 @@ def execute() {
             }
         }
     }
-    
 
     def _installUITests = {
         stage('Tests - install ui tests') {
