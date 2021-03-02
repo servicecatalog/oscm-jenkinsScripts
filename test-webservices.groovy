69ee95c79e1fbf8bd1876b74e14018792c803501
cworf91 <christian.worf@est.fujitsu.com>
2021-03-02 12:03:14 +0100

    refactor

--- test-webservices.groovy
+++ test-webservices.groovy
@@ -6,53 +6,79 @@
  *                                                                           *
  ****************************************************************************/
 
-//**
- How to use:
- 1. Create a job of type Pipeline
- 2. As the pipeline script paste this script
- 3. Create the following parameters (adjust defaults as you wish):
+// How to use:
+// 1. Create a job of type Pipeline
+// 2. As the pipeline script paste this script
+// 3. Create the following string parameters (adjust defaults if you wish):
+// --
+// Name: DOCKER_REGISTRY
+// Description: Registry host and port for the final Docker image. Example: DOCKER_REGISTRY/DOCKER_ORGANIZATION/oscm-core:TAG_DOCKER
+// --
+// Name: DOCKER_ORGANIZATION
+// Default: oscmdocker
+// Description: Organization name for the final Docker image. Example: DOCKER_REGISTRY/DOCKER_ORGANIZATION/oscm-core:TAG_DOCKER
+// --
+// Name: TAG_REPO_JENKINS_SCRIPTS
+// Default: master
+// Description: http://estscm1.intern.est.fujitsu.com/fujitsu-bss/jenkinsScripts
+// --
+// Name: TAG_REPO_DEVELOPMENT
+// Default: master
+// Description: Branch or tag in the git repository development
+// --
+// Name: TAG_REPO_DOCKERBUILD
+// Default: master
+// Description: Branch or tag in the git repository oscmaas-dockerbuild
+// --
+// Name: TAG_REPO_DOCUMENTATION
+// Default: master
+// Description: Branch or tag in the git repository documentation https://github.com/servicecatalog/documentation
+// --
+// Name: DOCKER_ORGANIZATION
+// Default: oscmdocker
+// Description: Project name for the final Docker image. Example: DOCKER_REGISTRY/DOCKER_ORGANIZATION/oscm-core:TAG_DOCKER
+// --
+// Name: TAG_DOCKER
+// Default: ws-tests
+// Description: Docker tag name for the built images
+// --
+// Name: ANT_URL
+// Default: https://archive.apache.org/dist/ant/binaries/apache-ant-1.9.4-bin.tar.gz
+// Description: URL to download ant from.
+// --
+// Name: IVY_URL
+// Default: http://www-eu.apache.org/dist//ant/ivy/2.4.0/apache-ivy-2.4.0-bin.tar.gz
+// Description: URL to download ivy from.
+// --
+// Name: OSCM_GIT_REPO
+// Default: https://github.com/servicecatalog/oscm.git
+// Description: Repository URL to download the oscm from.
+// --
+// Name: JAVA_HOME_DIR
+// Default: /etc/alternatives/java_sdk_1.8.0
+// Description: Path to the JDK directory.
+//
+// Create the following Node parameter:
+// --
+// Name: NODE_NAME
+// Select the default nodes and possible nodes for your environment
+// Enable "Disallow multi node selection"
+// Node eligibility: All nodes
+// Description: Where to set up the OSCM?
+//
+// Create the following Boolean parameters:
+// --
+// Name: PURGE_DATABASE
+// Default: disabled
+// Description: Delete the database and start with a fresh one?
+// --
+// Name: TOMEE_DEBUG
+// Default: disabled
 
- Required environment variables:
- ===
- Name: NODE_NAME
- Type: Option list
- Default: -
- Description: Nodes used to run the pipeline
- ===
- Name: DOCKER_TAG
- Type: String
- Default: latest
- Description: Docker tag name for created images
- ===
- Name: DOCKER_REGISTRY
- Type: String
- Description: Registry host and port for the final Docker image. Example: <DOCKER_REGISTRY>/<DOCKER_ORGANIZATION>/oscm-core:<DOCKER_TAG>
- ===
- Name: DOCKER_ORGANIZATION
- Type: String
- Default: oscmdocker
- Description: Organization name for the final Docker image. Example: <DOCKER_REGISTRY>/<DOCKER_ORGANIZATION>/oscm-core:<DOCKER_TAG>
- ===
- Name: SAMPLE_DATA
- Type: boolean
- Default: false
- Description: Load sample data?
- ===
- Name: TOMEE_DEBUG
- Type: boolean
- Default: false
- Description: Value ignored when glassfish chosen.
- ===
- Name: AUTH_MODE
- Type: Option list (INTERNAL, OIDC)
- Default: INTERNAL
- Description: Auth mode used to communicate with OSCM web services.
- ===
- Name: COMPLETE_CLEANUP
- Default: enabled
- Description: Delete the database and whole workspace
- ===
-**/
+// --
+// Name: SAMPLE_DATA
+// Default: disabled
+// Description: Load sample data?
 
 node("${NODE_NAME}") {
     
