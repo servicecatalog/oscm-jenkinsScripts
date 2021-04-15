2bf8ebf546049de8f62f109df223c286e3370e49
cworf91 <christian.worf@est.fujitsu.com>
2021-04-15 11:43:47 +0200

    refactor

--- shared/build-core.groovy
+++ shared/build-core.groovy
@@ -268,7 +268,7 @@ def execute() {
                     "-e https_proxy=\"${https_proxy}\" " +
                     "-e HTTP_PROXY=\"${http_proxy}\" " +
                     "-e HTTPS_PROXY=\"${https_proxy}\" " +
-                    "-e MAVEN_OPTS=\"-Duser.home=/build -Dhttp.proxyHost=proxy.intern.est.fujitsu.com -Dhttp.proxyPort=8080 -Dhttps.proxyHost=proxy.intern.est.fujitsu.com -Dhttps.proxyPort=8080\" " +
+                    "-e MAVEN_OPTS=\"-Xmx512m -Duser.home=/build -Dhttp.proxyHost="\"${http_proxy}\" -Dhttp.proxyPort="\"${http_port}\"  -DDhttps.proxyHost=\"${https_proxy}\" -Dhttps.proxyPort="\"${https_port}\" \" " +
                     "oscm-maven clean install -f /build/oscm-app-maven/pom.xml"
         }
     }
