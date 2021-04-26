57498ac9d01a2b7f54a49fe5424d11e4c6311596
cworf91 <christian.worf@est.fujitsu.com>
2021-04-26 17:08:58 +0200

    refactor

--- shared/proxyvariables.groovy
+++ shared/proxyvariables.groovy
@@ -16,14 +16,14 @@ void execute() {
             if [ -z ${http_proxy} && -z ${https_proxy} && -z ${http_port} && -z ${https_port}]; then
                 $OPTS = -Dhttp.proxyHost=\" ${http_proxy}\" -Dhttp.proxyPort=\"${http_port}\" -Dhttps.proxyHost=\"${https_proxy}\" -Dhttps.proxyPort=\"${https_port}\"
             elif [ -z ${http_proxy}  -z ${http_port} ]; then
-                $OPTS = \"-Dhttp.proxyHost=\" ${http_proxy}\" -Dhttp.proxyPort=\"${http_port}\" 
+                $OPTS = -Dhttp.proxyHost=\" ${http_proxy}\" -Dhttp.proxyPort=\"${http_port}\" 
             elif [ -z ${https_proxy} && -z ${https_port}]; then
                 $OPTS = -Dhttps.proxyHost=\"${https_proxy}\" -Dhttps.proxyPort=\"${https_port}\"
             else
                 $OPTS = ""
             fi;
             '''
-          env.PROXY_OPTS = OPTS
+          env.PROXY_OPTS = $OPTS
        }
        }
    }             
