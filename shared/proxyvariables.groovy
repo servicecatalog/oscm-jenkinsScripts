d08ba7e423f92319cfa4f166bbc525fcdf7fc1f7
cworf91 <christian.worf@est.fujitsu.com>
2021-04-26 16:52:32 +0200

    add proxyvariables

---
+++ shared/proxyvariables.groovy
@@ -0,0 +1,31 @@
+/*****************************************************************************
+ *                                                                           *
+ * Copyright FUJITSU LIMITED 2021                                            *
+ *                                                                           *
+ * Creation Date: 26-04-2021                                                 *
+ *                                                                           *
+ ****************************************************************************/
+ 
+void execute() {
+
+
+    def _setProxyVariables = {
+        stage('Build - set proxy variables') {
+        script {
+         sh '''
+            if [ -z ${http_proxy} && -z ${https_proxy} && -z ${http_port} && -z ${https_port}]; then
+                $OPTS = -Dhttp.proxyHost=\" ${http_proxy}\" -Dhttp.proxyPort=\"${http_port}\" -Dhttps.proxyHost=\"${https_proxy}\" -Dhttps.proxyPort=\"${https_port}\"
+            elif [ -z ${http_proxy}  -z ${http_port} ]; then
+                $OPTS = \"-Dhttp.proxyHost=\" ${http_proxy}\" -Dhttp.proxyPort=\"${http_port}\" 
+            elif [ -z ${https_proxy} && -z ${https_port}]; then
+                $OPTS = -Dhttps.proxyHost=\"${https_proxy}\" -Dhttps.proxyPort=\"${https_port}\"
+            else
+                $OPTS = ""
+            fi;
+            '''
+          env.PROXY_OPTS = OPTS
+     }
+                
+    _setProxyVariables()
+}
+return this
