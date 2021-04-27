ca868249b2bfd98c98c52cd32fb076ab7e8f5ceb
cworf91 <christian.worf@est.fujitsu.com>
2021-04-27 14:07:58 +0200

    fix

--- tests/portal-integration-tests.groovy
+++ tests/portal-integration-tests.groovy
@@ -84,7 +84,7 @@ def execute() {
                         "-e https_proxy=\"${https_proxy}\" " +
                         "-e HTTP_PROXY=\"${http_proxy}\" " +
                         "-e HTTPS_PROXY=\"${https_proxy}\" " +
-                        "-e MAVEN_OPTS=\"-Xmx512m -Duser.home=/build -Dhttp.proxyHost=\"${http_proxy}\" -Dhttp.proxyPort=\"${http_port}\"  -Dhttps.proxyHost=\"${https_proxy}\" -Dhttps.proxyPort=\"${https_port}\" \" " +
+                        "-e MAVEN_OPTS=\"-Xmx512m -Duser.home=/build -Dhttp.proxyHost=\"${HTTP_PROXY}\" -Dhttp.proxyPort=\"${HTTP_PORT}\"  -Dhttps.proxyHost=\"${HTTPs_PROXY}\" -Dhttps.proxyPort=\"${HTTPs_PORT}\" \" " +
                         "oscm-maven clean install -e -f /build/oscm-ui-tests/pom.xml"
             }
         }
