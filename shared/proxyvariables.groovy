5bac7ecbb979c0491b8139431fb909194943393e
cworf91 <christian.worf@est.fujitsu.com>
2021-04-28 17:46:06 +0200

    add user home

--- shared/proxyvariables.groovy
+++ shared/proxyvariables.groovy
@@ -20,15 +20,15 @@ void execute() {
                 def httpsHost = httpsUri?.getHost()
                 def httpsPort = httpsUri?.getPort()
                 env.ANT_OPTS="-Dhttp.proxyHost=${httpHost} -Dhttp.proxyPort=${httpPort} -Dhttps.proxyHost=${httpsHost} -Dhttps.proxyPort=${httpsPort}"
-                env.MAVEN_OPTS="-Xmx512m -Dhttp.proxyHost=${httpHost} -Dhttp.proxyPort=${httpPort} -Dhttps.proxyHost=${httpsHost} -Dhttps.proxyPort=${httpsPort}"
+                env.MAVEN_OPTS="-Xmx512m -Duser.home=/build -Dhttp.proxyHost=${httpHost} -Dhttp.proxyPort=${httpPort} -Dhttps.proxyHost=${httpsHost} -Dhttps.proxyPort=${httpsPort}"
             } else if ( "${http_proxy}" != '') {
                 URI httpUri = new URI("${http_proxy}") 
                 env.ANT_OPTS="-Dhttp.proxyHost=httpUri?.getHost() -Dhttp.proxyPort=httpUri?.getPort()"
-                env.MAVEN_OPTS="-Xmx512m -Dhttp.proxyHost=httpUri?.getHost() -Dhttp.proxyPort=httpUri?.getPort()"
+                env.MAVEN_OPTS="-Xmx512m -Duser.home=/build -Dhttp.proxyHost=httpUri?.getHost() -Dhttp.proxyPort=httpUri?.getPort()"
             } else if ( "${https_proxy}" != '' ) {
                 URI httpsUri = new URI("${https_proxy}") 
                 env.ANT_OPTS="-Dhttps.proxyHost=httpsUri?.getHost() -Dhttps.proxyPort=httspUri?.getPort()"
-                env.MAVEN_OPTS="-Xmx512m -Dhttp.proxyHost= -Dhttps.proxyHost=httpsUri?.getHost() -Dhttps.proxyPort=httspUri?.getPort()"
+                env.MAVEN_OPTS="-Xmx512m -Duser.home=/build -Dhttps.proxyHost=httpsUri?.getHost() -Dhttps.proxyPort=httspUri?.getPort()"
            } else {
                 env.ANT_OPTS=""
                 env.MAVEN_OPTS="-Xmx512m -Duser.home=/build"
