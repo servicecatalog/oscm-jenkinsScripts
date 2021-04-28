59d97991173d47bcbed1a1a6260ceb70594e053f
cworf91 <christian.worf@est.fujitsu.com>
2021-04-28 12:20:04 +0200

    add evnivroment variables

--- shared/proxyvariables.groovy
+++ shared/proxyvariables.groovy
@@ -10,12 +10,21 @@ void execute() {
 
 
     def _setProxyVariables = {
+    
+    environment {
+        env.ANT_OPTS
+        env.MAVEN_OPTS
+    }
         stage('Build - set proxy variables') {
         script {
-        env.PROXY_OPTS
+        env.ANT_OPTS
+        env.MAVEN_OPTS
          sh '''
             if [ -n ${http_proxy} ] && [ -n ${https_proxy} ] && [ -n ${http_port} ] && [ -n ${https_port} ]; then
                PROXY_OPTS="-Dhttp.proxyHost=\"${http_proxy}\" -Dhttp.proxyPort=\"${http_port}\" -Dhttps.proxyHost=\"${https_proxy}\" -Dhttps.proxyPort=\"${https_port}\""
+               
+               ANT_OPTS="-Dhttp.proxyHost=proxy.intern.est.fujitsu.com -Dhttp.proxyPort=8080 -Dhttps.proxyHost=proxy.intern.est.fujitsu.com -Dhttps.proxyPort=8080"
+               MAVEN_OPTS"-Xmx512m -Duser.home=/build -Dhttp.proxyHost=proxy.intern.est.fujitsu.com -Dhttp.proxyPort=8080 -Dhttps.proxyHost=proxy.intern.est.fujitsu.com -Dhttps.proxyPort=8080"
             elif [ -n ${http_proxy} ] && [ -n ${http_port} ]; then
                 PROXY_OPTS="-Dhttp.proxyHost=\"${http_proxy}\" -Dhttp.proxyPort=\"${http_port}\""
             elif [ -n ${https_proxy} ] && [ -n ${https_port} ]; then
