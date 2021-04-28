19e193f52ba02b5982c6f0a74a56bb1403f5d651
cworf91 <christian.worf@est.fujitsu.com>
2021-04-28 16:11:36 +0200

    use other syntax

--- shared/proxyvariables.groovy
+++ shared/proxyvariables.groovy
@@ -16,8 +16,8 @@ void execute() {
 	        	String[] http;
 	        	String[] https;
 	        if ( "${http_proxy}" != ''  && "${https_proxy}" != '') {
-      			http = http_proxy.split(':');
-      			https = https_proxy.split(':');
+      			http = "${http_proxy}".split(':');
+      			https = "${https_proxy}".split(':');
                env.ANT_OPTS="-Dhttp.proxyHost=\"${http[0]}\" -Dhttp.proxyPort=\"${http[1]}\" -Dhttps.proxyHost=\"${https[0]}\" -Dhttps.proxyPort=\"${https[1]}\""
                env.MAVEN_OPTS="-Xmx512m -Duser.home=/build -Dhttp.proxyHost=\"${http[0]}\" -Dhttp.proxyPort=\"${http[1]}\" -Dhttps.proxyHost=\"${https[0]}\" -Dhttps.proxyPort=\"${https[1]}\""
             } else if ( "${http_proxy}" != '' && "${http_port}" != '' ) {
