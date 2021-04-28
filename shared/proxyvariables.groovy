ec1b4862e597d18d20d4bd201ed74e85701351a9
cworf91 <christian.worf@est.fujitsu.com>
2021-04-28 16:34:27 +0200

    use URI method

--- shared/proxyvariables.groovy
+++ shared/proxyvariables.groovy
@@ -13,12 +13,10 @@ void execute() {
     
         stage('Build - set proxy variables') {
 	        script {
-                String[] http;
-                String[] https;
 	        if ( "${http_proxy}" != ''  && "${https_proxy}" != '') {
-                http = "${http_proxy}".split(':');
-                https = "${https_proxy}".split(':');
-                env.ANT_OPTS="-Dhttp.proxyHost=\"${http[0]}\" -Dhttp.proxyPort=\"${http[1]}\" -Dhttps.proxyHost=\"${https[0]}\" -Dhttps.proxyPort=\"${https[1]}\""
+                URI httpUri = new URI("${http_proxy}") 
+                URI httpsUri = new URI("${https_proxy}") 
+                env.ANT_OPTS="-Dhttp.proxyHost=\"httpUri?.getHost()\" -Dhttp.proxyPort=\"httpUri?.getPort()\" -Dhttps.proxyHost=\"httpsUri?.getHost()}\" -Dhttps.proxyPort=\"httpUri?.getPort()\""
                 env.MAVEN_OPTS="-Xmx512m -Duser.home=/build -Dhttp.proxyHost=\"${http[0]}\" -Dhttp.proxyPort=\"${http[1]}\" -Dhttps.proxyHost=\"${https[0]}\" -Dhttps.proxyPort=\"${https[1]}\""
             } else if ( "${http_proxy}" != '') {
                 http = "${http_proxy}".split(':');
