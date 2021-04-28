f2897fd84784c697f2c04b506b15a84c3f3c9e1c
cworf91 <christian.worf@est.fujitsu.com>
2021-04-28 14:50:43 +0200

    add ""

--- shared/proxyvariables.groovy
+++ shared/proxyvariables.groovy
@@ -13,13 +13,13 @@ void execute() {
     
         stage('Build - set proxy variables') {
 	        script {
-	        if ( ${http_proxy} != ''  && ${https_proxy} != '' && ${http_port} != '' &&  ${https_port} != '' ) {
+	        if ( "${http_proxy}" != ''  && "${https_proxy}" != '' && "${http_port}" != '' &&  "${https_port}" != '' ) {
                env.ANT_OPTS="-Dhttp.proxyHost=\"${http_proxy}\" -Dhttp.proxyPort=\"${http_port}\" -Dhttps.proxyHost=\"${https_proxy}\" -Dhttps.proxyPort=\"${https_port}\""
                env.MAVEN_OPTS="-Xmx512m -Duser.home=/build -Dhttp.proxyHost=\"${http_proxy}\" -Dhttp.proxyPort=\"${http_port}\" -Dhttps.proxyHost=\"${https_proxy}\" -Dhttps.proxyPort=\"${https_port}\""
-            } else if ( "${http_proxy}" != '' && ${http_port} != '' ) {
+            } else if ( "${http_proxy}" != '' && "${http_port}" != '' ) {
                env.ANT_OPTS="-Dhttp.proxyHost=\"${http_proxy}\" -Dhttp.proxyPort=\"${http_port}\""
                env.MAVEN_OPTS="-Xmx512m -Duser.home=/build -Dhttp.proxyHost=\"${http_proxy}\" -Dhttp.proxyPort=\"${http_port}\""
-            } else if ( ${https_proxy} != '' &&  ${https_port} != '' ) {
+            } else if ( "${https_proxy}" != '' &&  "${https_port}" != '' ) {
                env.ANT_OPTS=" -Dhttps.proxyHost=\"${https_proxy}\" -Dhttps.proxyPort=\"${https_port}\""
                env.MAVEN_OPTS="-Xmx512m -Duser.home=/build -Dhttps.proxyHost=\"${https_proxy}\" -Dhttps.proxyPort=\"${https_port}\""
 	        } else {
