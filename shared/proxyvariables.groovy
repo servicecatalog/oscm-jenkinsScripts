7feb9c33d3f89cdd387d8a18f91d1f96cab601de
cworf91 <christian.worf@est.fujitsu.com>
2021-04-28 12:42:30 +0200

    add echo

--- shared/proxyvariables.groovy
+++ shared/proxyvariables.groovy
@@ -11,14 +11,10 @@ void execute() {
 
     def _setProxyVariables = {
     
-    environment {
-        env.ANT_OPTS
-        env.MAVEN_OPTS
-    }
         stage('Build - set proxy variables') {
         script {
-        env.ANT_OPTS
-        env.MAVEN_OPTS
+        ANT_OPTS
+        MAVEN_OPTS
          sh '''
             if [ -n ${http_proxy} ] && [ -n ${https_proxy} ] && [ -n ${http_port} ] && [ -n ${https_port} ]; then
                PROXY_OPTS="-Dhttp.proxyHost=\"${http_proxy}\" -Dhttp.proxyPort=\"${http_port}\" -Dhttps.proxyHost=\"${https_proxy}\" -Dhttps.proxyPort=\"${https_port}\""
@@ -27,15 +23,31 @@ void execute() {
                MAVEN_OPTS="-Xmx512m -Duser.home=/build -Dhttp.proxyHost=proxy.intern.est.fujitsu.com -Dhttp.proxyPort=8080 -Dhttps.proxyHost=proxy.intern.est.fujitsu.com -Dhttps.proxyPort=8080"
             elif [ -n ${http_proxy} ] && [ -n ${http_port} ]; then
                 PROXY_OPTS="-Dhttp.proxyHost=\"${http_proxy}\" -Dhttp.proxyPort=\"${http_port}\""
+                ANT_OPTS="-Dhttp.proxyHost=proxy.intern.est.fujitsu.com -Dhttp.proxyPort=8080 -Dhttps.proxyHost=proxy.intern.est.fujitsu.com -Dhttps.proxyPort=8080"
+               MAVEN_OPTS="-Xmx512m -Duser.home=/build -Dhttp.proxyHost=proxy.intern.est.fujitsu.com -Dhttp.proxyPort=8080 -Dhttps.proxyHost=proxy.intern.est.fujitsu.com -Dhttps.proxyPort=8080"
             elif [ -n ${https_proxy} ] && [ -n ${https_port} ]; then
                 PROXY_OPTS="-Dhttps.proxyHost=\"${https_proxy}\" -Dhttps.proxyPort=\"${https_port}\""
+                ANT_OPTS="-Dhttp.proxyHost=proxy.intern.est.fujitsu.com -Dhttp.proxyPort=8080 -Dhttps.proxyHost=proxy.intern.est.fujitsu.com -Dhttps.proxyPort=8080"
+               MAVEN_OPTS="-Xmx512m -Duser.home=/build -Dhttp.proxyHost=proxy.intern.est.fujitsu.com -Dhttp.proxyPort=8080 -Dhttps.proxyHost=proxy.intern.est.fujitsu.com -Dhttps.proxyPort=8080"
             else
                PROXY_OPTS=""
+               ANT_OPTS=""
+               MAVEN_OPTS=""
             fi;
             '''
        }
        }
    }             
+     def _echo = {
+          stage('Print Varliabe'){
+            steps{
+                echo ANT_OPTS
+                echo MAVEN_OPTS
+            }
+        }
+    }
+    
     _setProxyVariables()
+    _echo
 }
 return this
