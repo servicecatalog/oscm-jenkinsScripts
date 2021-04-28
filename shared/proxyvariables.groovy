d4e0261ddc0da5e35d895b87cfaff9a13af3a271
cworf91 <christian.worf@est.fujitsu.com>
2021-04-28 14:35:15 +0200

    remove ""

--- shared/proxyvariables.groovy
+++ shared/proxyvariables.groovy
@@ -13,7 +13,7 @@ void execute() {
     
         stage('Build - set proxy variables') {
 	        script {
-	        if ( "${http_proxy}" != ''  && ${https_proxy} != '' && ${http_port} != '' &&  ${https_port} != '' ) {
+	        if ( ${http_proxy} != ''  && ${https_proxy} != '' && ${http_port} != '' &&  ${https_port} != '' ) {
                env.ANT_OPTS="-Dhttp.proxyHost=\"${http_proxy}\" -Dhttp.proxyPort=\"${http_port}\" -Dhttps.proxyHost=\"${https_proxy}\" -Dhttps.proxyPort=\"${https_port}\""
                env.MAVEN_OPTS="-Xmx512m -Duser.home=/build -Dhttp.proxyHost=\"${http_proxy}\" -Dhttp.proxyPort=\"${http_port}\" -Dhttps.proxyHost=\"${https_proxy}\" -Dhttps.proxyPort=\"${https_port}\""
             } else if ( "${http_proxy}" != '' && ${http_port} != '' ) {
