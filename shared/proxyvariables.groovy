e080df674381c430b0b67f1c81e6a08d7bfad67b
cworf91 <christian.worf@est.fujitsu.com>
2021-04-28 14:28:37 +0200

    add breaked

--- shared/proxyvariables.groovy
+++ shared/proxyvariables.groovy
@@ -16,7 +16,7 @@ void execute() {
 	        if ( "${http_proxy}" != ''  && ${https_proxy} != '' && ${http_port} != '' &&  ${https_port} != '' ) {
                env.ANT_OPTS="-Dhttp.proxyHost=\"${http_proxy}\" -Dhttp.proxyPort=\"${http_port}\" -Dhttps.proxyHost=\"${https_proxy}\" -Dhttps.proxyPort=\"${https_port}\""
                env.MAVEN_OPTS="-Xmx512m -Duser.home=/build -Dhttp.proxyHost=\"${http_proxy}\" -Dhttp.proxyPort=\"${http_port}\" -Dhttps.proxyHost=\"${https_proxy}\" -Dhttps.proxyPort=\"${https_port}\""
-            } else if ( "${http_proxy}" != '' && ${http_port} != '' ) 
+            } else if ( "${http_proxy}" != '' && ${http_port} != '' ) {
                env.ANT_OPTS="-Dhttp.proxyHost=\"${http_proxy}\" -Dhttp.proxyPort=\"${http_port}\""
                env.MAVEN_OPTS="-Xmx512m -Duser.home=/build -Dhttp.proxyHost=\"${http_proxy}\" -Dhttp.proxyPort=\"${http_port}\""
             } else if ( ${https_proxy} != '' &&  ${https_port} != '' ) {
