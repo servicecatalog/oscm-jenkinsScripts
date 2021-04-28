ef7191ce29b7d07508c56a95e793957e1143c678
cworf91 <christian.worf@est.fujitsu.com>
2021-04-28 16:16:00 +0200

    adapt script

--- shared/proxyvariables.groovy
+++ shared/proxyvariables.groovy
@@ -13,22 +13,24 @@ void execute() {
     
         stage('Build - set proxy variables') {
 	        script {
-	        	String[] http;
-	        	String[] https;
+                String[] http;
+                String[] https;
 	        if ( "${http_proxy}" != ''  && "${https_proxy}" != '') {
-      			http = "${http_proxy}".split(':');
-      			https = "${https_proxy}".split(':');
-               env.ANT_OPTS="-Dhttp.proxyHost=\"${http[0]}\" -Dhttp.proxyPort=\"${http[1]}\" -Dhttps.proxyHost=\"${https[0]}\" -Dhttps.proxyPort=\"${https[1]}\""
-               env.MAVEN_OPTS="-Xmx512m -Duser.home=/build -Dhttp.proxyHost=\"${http[0]}\" -Dhttp.proxyPort=\"${http[1]}\" -Dhttps.proxyHost=\"${https[0]}\" -Dhttps.proxyPort=\"${https[1]}\""
-            } else if ( "${http_proxy}" != '' && "${http_port}" != '' ) {
-               env.ANT_OPTS="-Dhttp.proxyHost=\"${http_proxy}\" -Dhttp.proxyPort=\"${http_port}\""
-               env.MAVEN_OPTS="-Xmx512m -Duser.home=/build -Dhttp.proxyHost=\"${http_proxy}\" -Dhttp.proxyPort=\"${http_port}\""
+                http = "${http_proxy}".split(':');
+                https = "${https_proxy}".split(':');
+                env.ANT_OPTS="-Dhttp.proxyHost=\"${http[0]}\" -Dhttp.proxyPort=\"${http[1]}\" -Dhttps.proxyHost=\"${https[0]}\" -Dhttps.proxyPort=\"${https[1]}\""
+                env.MAVEN_OPTS="-Xmx512m -Duser.home=/build -Dhttp.proxyHost=\"${http[0]}\" -Dhttp.proxyPort=\"${http[1]}\" -Dhttps.proxyHost=\"${https[0]}\" -Dhttps.proxyPort=\"${https[1]}\""
+            } else if ( "${http_proxy}" != '') {
+                http = "${http_proxy}".split(':');
+                env.ANT_OPTS="-Dhttp.proxyHost=\"${http[0]}\" -Dhttp.proxyPort=\"${http[1]}\""
+                env.MAVEN_OPTS="-Xmx512m -Duser.home=/build -Dhttp.proxyHost=\"${http[0]}\" -Dhttp.proxyPort=\"${http[1]}\""
             } else if ( "${https_proxy}" != '' &&  "${https_port}" != '' ) {
-               env.ANT_OPTS=" -Dhttps.proxyHost=\"${https_proxy}\" -Dhttps.proxyPort=\"${https_port}\""
-               env.MAVEN_OPTS="-Xmx512m -Duser.home=/build -Dhttps.proxyHost=\"${https_proxy}\" -Dhttps.proxyPort=\"${https_port}\""
-	        } else {
-	           env.ANT_OPTS=""
-	           env.MAVEN_OPTS="-Xmx512m -Duser.home=/build"
+                https = "${https_proxy}".split(':');
+                env.ANT_OPTS=" -Dhttps.proxyHost=\"${https[0]}\" -Dhttps.proxyPort=\"${https[1]}\""
+                env.MAVEN_OPTS="-Xmx512m -Duser.home=/build -Dhttps.proxyHost=\"${https[0]}\" -Dhttps.proxyPort=\"${https[1]}\""
+           } else {
+                env.ANT_OPTS=""
+                env.MAVEN_OPTS="-Xmx512m -Duser.home=/build"
 	        }       
 	       }
        }
