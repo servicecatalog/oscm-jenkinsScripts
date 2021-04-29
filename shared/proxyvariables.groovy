75dfeb2c0fa4769a8eaf8a67203b9e55f233e0b1
cworf91 <christian.worf@est.fujitsu.com>
2021-04-29 13:53:01 +0200

    refactor

--- shared/proxyvariables.groovy
+++ shared/proxyvariables.groovy
@@ -5,13 +5,7 @@
  * Creation Date: 26-04-2021                                                 *
  *                                                                           *
  ****************************************************************************/
-                  def getPort(String[] proxy) {
-                   def port
-                   if( proxy.length > 1) {
-                     port = proxy[1]
-                }
-                return port
-                } 
+
 void execute() {
 
 
@@ -24,18 +18,12 @@ void execute() {
                 env.BUILD_PROXY_ARGS="--build-arg http_proxy=\"${http_proxy}\" --build-arg https_proxy=\"${https_proxy}\" --build-arg HTTP_PROXY=\"${http_proxy}\" --build-arg HTTPS_PROXY=\"${https_proxy}\" " 
                 
 	        if ( "${http_proxy}" != ''  && "${https_proxy}" != '') {
-      			http = "${http_proxy}".replaceAll(".*://", "").split(':');
-      			https = "${https_proxy}".replaceAll(".*://", "").split(':');
+      			http = splitProxy("${http_proxy}")
+      			https = splitProxy("${https_proxy}")
                 def httpHost = http[0]
                 def httpsHost = https[0]
                 def httpPort = getPort(http)
                 def httpsPort = getPort(https)
-                if( http.length > 1) {
-                    http[1]
-                }
-                if( https.length > 1) {
-                    https[1]
-                }
                 env.ANT_OPTS="-Dhttp.proxyHost=${httpHost} -Dhttp.proxyPort=${httpPort} -Dhttps.proxyHost=${httpsHost} -Dhttps.proxyPort=${httpsPort}"
                 env.MAVEN_OPTS="-Xmx512m -Duser.home=/build -Dhttp.proxyHost=${httpHost} -Dhttp.proxyPort=${httpPort} -Dhttps.proxyHost=${httpsHost} -Dhttps.proxyPort=${httpsPort}"
             } else if ( "${http_proxy}" != '') {
@@ -59,4 +47,16 @@ void execute() {
    
     _setProxyVariables()
 }
+
+def getPort(String[] proxy) {
+    def port
+    if( proxy.length > 1) {
+        port = proxy[1]
+    }
+    return port
+} 
+
+def splitProxy(String proxy) {
+    return proxy.replaceAll(".*://", "").split(':');
+} 
 return this
