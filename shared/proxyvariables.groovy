7cb84245651d098651fd35f3d6a35a234597497c
cworf91 <christian.worf@est.fujitsu.com>
2021-04-29 13:58:43 +0200

    refactor

--- shared/proxyvariables.groovy
+++ shared/proxyvariables.groovy
@@ -13,14 +13,15 @@ void execute() {
     
 
         stage('Build - set proxy variables') { 
-              	http = splitProxy("${http_proxy}")
-      			https = splitProxy("${https_proxy}")
-                def httpHost = http[0]
-                def httpsHost = https[0]
-                def httpPort = getPort(http)
-                def httpsPort = getPort(https)
-                env.RUN_PROXY_ARGS ="-e http_proxy=\"${http_proxy}\" -e https_proxy=\"${https_proxy}\" -e HTTP_PROXY=\"${http_proxy}\" -e HTTPS_PROXY=\"${https_proxy}\""
-                env.BUILD_PROXY_ARGS="--build-arg http_proxy=\"${http_proxy}\" --build-arg https_proxy=\"${https_proxy}\" --build-arg HTTP_PROXY=\"${http_proxy}\" --build-arg HTTPS_PROXY=\"${https_proxy}\" " 
+            http = splitProxy("${http_proxy}")
+            https = splitProxy("${https_proxy}")
+            def httpHost = http[0]
+            def httpsHost = https[0]
+            def httpPort = getPort(http)
+            def httpsPort = getPort(https)
+                
+            env.RUN_PROXY_ARGS ="-e http_proxy=\"${http_proxy}\" -e https_proxy=\"${https_proxy}\" -e HTTP_PROXY=\"${http_proxy}\" -e HTTPS_PROXY=\"${https_proxy}\""
+            env.BUILD_PROXY_ARGS="--build-arg http_proxy=\"${http_proxy}\" --build-arg https_proxy=\"${https_proxy}\" --build-arg HTTP_PROXY=\"${http_proxy}\" --build-arg HTTPS_PROXY=\"${https_proxy}\" " 
                 
 	        if ( "${http_proxy}" != ''  && "${https_proxy}" != '') {
                 env.ANT_OPTS="-Dhttp.proxyHost=${httpHost} -Dhttp.proxyPort=${httpPort} -Dhttps.proxyHost=${httpsHost} -Dhttps.proxyPort=${httpsPort}"
