36f207b3505546634f35e4a77c7c693c29178c90
cworf91 <christian.worf@est.fujitsu.com>
2021-04-29 13:46:03 +0200

    change scope

--- shared/proxyvariables.groovy
+++ shared/proxyvariables.groovy
@@ -8,16 +8,16 @@
  
 void execute() {
 
-             def getPort(String[] proxy) {
+
+    def _setProxyVariables = {
+    
+                 def getPort(String[] proxy) {
                    def port
                    if( proxy.length > 1) {
                      port = proxy[1]
                 }
                 return port
                 } 
-
-    def _setProxyVariables = {
-    
         stage('Build - set proxy variables') { 
         
                 env.RUN_PROXY_ARGS ="-e http_proxy=\"${http_proxy}\" -e https_proxy=\"${https_proxy}\" -e HTTP_PROXY=\"${http_proxy}\" -e HTTPS_PROXY=\"${https_proxy}\""
