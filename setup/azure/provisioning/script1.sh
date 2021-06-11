f1de648e756ec6c787be815f6afe1282e3fa3f81
cworf91 <christian.worf@est.fujitsu.com>
2021-06-11 17:10:32 +0200

    change to ubuntu

--- setup/azure/provisioning/script1.sh
+++ setup/azure/provisioning/script1.sh
@@ -1,10 +1,19 @@
 echo "Getting epel-release package available"
 
-yum install epel-release -y
+apt-get install build-essential libssl-dev libffi-dev python-dev -y
 
-echo "Getting ansible installed"
+apt-get update
+apt-get install python3-pip -y
 
-yum install ansible -y
+pip3 install --upgrade pip
+pip3 install ansible[azure]==2.10.7
+
+curl -O https://raw.githubusercontent.com/ansible-collections/azure/dev/requirements-azure.txt
+pip3 install -r requirements-azure.txt
+rm requirements-azure.txt
+
+ansible-galaxy collection install azure.azcollection 
+ansible-galaxy collection install community.general 
 
 echo "Executing ansible playbook"
 
