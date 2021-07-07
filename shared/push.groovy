5c897bf849f2ac86ab9096b2dd931ff8a4e36ed1
cworf91 <christian.worf@est.fujitsu.com>
2021-07-07 11:55:46 +0200

    refactor

--- shared/push.groovy
+++ shared/push.groovy
@@ -104,7 +104,7 @@ def execute(boolean loginRequired = false, publish = false, IMAGES) {
 
     def _loginToDst = {
        stage('Push - login to registry') {
-       if ("${USERNAME}" == null || "${USERNAME}" == "" || "${PASSWORD}" == null || "${PASSWORD}" == "") {
+       if (!binding.hasVariable('USERNAME') || !binding.hasVariable('PASSWORD')) {
            withCredentials([string(credentialsId: 'GIT-USERNAME', variable: 'USER'), string(credentialsId: 'GIT-PASSWORD', variable: 'PASS')]) {
        	      $USERNAME = ${USER}
        	      $PASSWORD = ${PASS}
